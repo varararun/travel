9d4a19ac0a5bfdd18148fee3c641eeb25e8d32fe
Arun Varghese <avarghese@spireon.com>
2016-12-05 14:18:00 -0600

    initial travel gallery commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Travel
+Travel Gallery Demo
---
+++ app.js
@@ -0,0 +1,513 @@
+new WOW().init();
+
+var Travel = {
+    maxPage: 15,
+    albumImages:[],
+	places: {
+    "Phuket": {
+        "name": "Phuket",
+        "date": "November 22nd 2016",
+        "description": "a description",
+        "thumbnail": "http://i.imgur.com/3ToI0jJh.jpg",
+        "rank": 5,
+        "tags": "",
+        "places": "",
+        "album": "http://imgur.com/a/cvkkT",
+        "blog":"http://avarghese.me/blog/travel/2016/11/19/thailand.html",
+        "albumId":"cvkkT"
+     },
+     "Bangkok": {
+        "name": "Bangkok",
+        "date": "November 19th 2016",
+        "description": "a description",
+        "thumbnail": "http://i.imgur.com/DZJB5Glh.jpg",
+        "rank": 5,
+        "tags": "",
+        "places": "",
+        "album": "http://imgur.com/a/4OJ0x",
+        "blog":"http://avarghese.me/blog/travel/2016/11/19/thailand.html",
+        "albumId":"4OJ0x"
+     },
+     "CostaRica": {
+        "name": "Costa Rica",
+        "date": "October 7th 2016",
+        "description": "a description",
+        "thumbnail": "http://i.imgur.com/KAYGUVah.jpg",
+        "rank": 10,
+        "tags": "",
+        "places": "",
+        "album": "http://imgur.com/a/Mv4CT",
+        "blog":"http://avarghese.me/blog/travel/2016/10/07/costa-rica.html",
+        "albumId":"Mv4CT"
+     },
+   	"BigSur": {
+        "name": "Big Sur",
+        "date": "September 23rd 2016",
+        "description": "a description",
+        "thumbnail": "http://i.imgur.com/Q0lyf12h.jpg",
+        "rank": 2,
+        "tags": "roadtrip,bigsur",
+        "places": "",
+        "album": "http://imgur.com/a/V3P0z",
+        "blog":"http://avarghese.me/blog/travel/2016/09/23/big-sur-roadtrip.html",
+        "albumId":"V3P0z"
+     },
+     "Negril": {
+        "name": "Negril",
+        "date": "August 26th 2016",
+        "description": "a description",
+        "thumbnail": "http://i.imgur.com/pawE6y2h.jpg",
+        "rank": 2,
+        "tags": "resort,jamaica,villas,cliffjump",
+        "places": "",
+        "album": "http://imgur.com/a/go1Dc",
+        "blog":"http://avarghese.me/blog/travel/2016/08/26/negril.html",
+        "albumId":"go1Dc"
+     },
+     "PuertoVallarta": {
+        "name": "Puerto Vallarta",
+        "date": "August 20th 2016",
+        "description": "a description",
+        "thumbnail": "http://i.imgur.com/tI4r2nNh.jpg",
+        "rank": 5,
+        "tags": "resort,mexico,infinitypool",
+        "places": "",
+        "album": "http://imgur.com/a/t4kRN",
+        "blog":"http://avarghese.me/blog/travel/2016/08/20/puerto-vallarta.html",
+        "albumId":"t4kRN"
+     },
+     "Chicago": {
+        "name": "Chicago",
+        "date": "August 6th 2016",
+        "description": "a description",
+        "thumbnail": "http://i.imgur.com/UURlnKlh.jpg",
+        "rank": 20,
+        "tags": "architecture,pier,cruise,pizza",
+        "places": "",
+        "album": "http://imgur.com/a/rCay6",
+        "blog":"",
+        "albumId":"rCay6"
+     },
+     "PuertoRico": {
+        "name":"Puerto Rico",
+        "date":"July 8th 2016",
+        "description":"a description",
+        "thumbnail":"http://imgur.com/R1KRe85h.jpg",
+        "rank": 1,
+        "tags": "architecture,hiking,towns,mofungo",
+        "places": "",
+        "album": "http://imgur.com/a/CWr4t",
+        "blog":"http://avarghese.me/blog/travel/2016/07/08/puerto-rico-trip.html",
+        "albumId":"CWr4t"
+      }, 
+     "SanFrancisco": {
+        "name":"San Francisco",
+        "date":"July 2nd 2016",
+        "description":"a description",
+        "thumbnail":"http://i.imgur.com/mQZnMf4h.jpg",
+        "rank": 20,
+        "tags": "walking,city,pier",
+        "places": "",
+        "album": "http://imgur.com/a/6CEOm",
+        "blog":"",
+        "albumId":"6CEOm"
+      }, 
+      "Barcelona": {
+        "name":"Spain",
+        "date":"May 13th 2016",
+        "description":"a description",
+        "thumbnail":"http://i.imgur.com/RLSRJv0h.jpg",
+        "rank": 1,
+        "tags": "architecture,hiking",
+        "places": "",
+        "album": "http://imgur.com/a/0Wx24",
+        "blog":"http://avarghese.me/blog/travel/2016/05/16/barcelona-spain-trip.html",
+        "albumId":"0Wx24"
+      }, 
+      "Italy": {
+        "name":"Italy",
+        "date":"May 4th 2016",
+        "description":"a description",
+        "thumbnail":"http://i.imgur.com/Z9dw87jh.jpg",
+        "rank": 1,
+        "tags": "countryside,hiking,city,gondolas",
+        "places": "",
+        "album": "http://imgur.com/a/z7iaO",
+        "blog":"http://avarghese.me/blog/travel/2016/05/15/italy-trip.html",
+        "albumId":"z7iaO"
+      },  
+      "Colorado": {
+        "name":"Colorado",
+        "date":"April 9th 2016",
+        "description":"a description",
+        "thumbnail":"http://i.imgur.com/QRil5OBh.jpg",
+        "rank": 1,
+        "tags": "tropical,resort,watersports",
+        "places": "",
+        "album": "http://imgur.com/a/GznDe",
+        "blog":"",
+        "albumId":"GznDe"
+      },        
+      "CaboSanLucas":{
+        "name":"Cabo San Lucas",
+        "date":"February 6th 2016",
+        "description":"a description",
+        "thumbnail":"http://i.imgur.com/PWBSQp9h.jpg",
+        "rank": 10,
+        "tags": "parasailing,tropical",
+        "places": "",
+        "album": "http://imgur.com/a/of5lB",
+        "blog":"",
+        "albumId":"of5lB"
+      },
+      "SaintLucia":{
+        "name":"Saint Lucia",
+        "date":"November 27th 2015",
+        "description":"a description",
+        "thumbnail":"http://i.imgur.com/3Jr1CVKh.jpg",
+        "rank": 1,
+        "tags": "tropical,resort,volcanicbath",
+        "places": "",
+        "album": "http://imgur.com/a/OZl4l",
+        "blog":"",
+        "albumId":"OZl4l"
+      },
+      "Cancun":{
+        "name":"Cancun",
+        "date":"November 13th 2015",
+        "description":"a description",
+        "thumbnail":"http://i.imgur.com/Vz7Yeoch.jpg",
+        "rank": 5,
+        "tags": "tropical,canyoneering",
+        "places": "",
+        "album": "http://imgur.com/a/Y1IKS",
+        "blog":"",
+        "albumId":"Y1IKS"
+      },
+      "Vancouver":{
+        "name":"Vancouver",
+        "date":"September 3rd 2015",
+        "description":"a description",
+        "thumbnail":"http://i.imgur.com/WtjP0CEh.jpg",
+        "rank": 10,
+        "tags": "cold,nature,hiking",
+        "places": "",
+        "album": "http://imgur.com/a/FrNOL",
+        "blog":"",
+        "albumId":"FrNOL"
+      },
+      "Paris":{
+        "name":"Paris",
+        "date":"June 21st 2015",
+        "description":"a description",
+        "thumbnail":"http://i.imgur.com/4o5ud9kh.jpg",
+        "rank": 10,
+        "tags": "city,tourism",
+        "places": "",
+        "album": "http://imgur.com/a/8fFJT",
+        "blog":"",
+        "albumId":"8fFJT"
+      },
+      "Maldives": {
+        "name":"Maldives",
+        "date":"June 16th 2015",
+        "description":"a description",
+        "thumbnail":"http://i.imgur.com/x8f8KgRh.jpg",
+        "rank": 1,
+        "tags": "tropical,resort,watersports",
+        "places": "",
+        "album": "http://imgur.com/a/AHnS7",
+        "blog":"",
+        "albumId":"AHnS7"
+      },
+      "EastCoastS":{
+        "name":"East Coast Roadtrip going South",
+        "date":"April 23rd 2015",
+        "description":"a description",
+        "thumbnail":"http://i.imgur.com/7iQpKc8h.jpg",
+        "rank": 10,
+        "tags": "east-coast,driving,roadtrip",
+        "places": "sanfrancisco,losangeles,sandiego",
+        "album": "http://imgur.com/a/03ibF",
+        "blog":"",
+        "albumId":"03ibF"
+      },
+      "Japan":{
+        "name":"Japan",
+        "date":"December 19th 2014",
+        "description":"a description",
+        "thumbnail":"http://i.imgur.com/sWsF4tMh.jpg",
+        "rank": 10,
+        "tags": "city",
+        "places": "",
+        "album": "http://imgur.com/a/wF1UV",
+        "blog":"",
+        "albumId":"wF1UV"
+      },
+      "EastCoastN":{
+        "name":"East Coast Roadtrip going North",
+        "date":"November 8th 2014",
+        "description":"a description",
+        "thumbnail":"http://i.imgur.com/Em5c4kdh.jpg",
+        "rank": 10,
+        "tags": "eastcoast,driving,roadtrip",
+        "places": "",
+        "album": "http://imgur.com/a/x8Onv",
+        "blog":"",
+        "albumId":"x8Onv"
+      },
+      "NewYork":{
+        "name":"New York",
+        "date":"August 30th 2014",
+        "description":"a description",
+        "thumbnail":"http://i.imgur.com/e8s5F8xh.jpg",
+        "rank": 20,
+        "tags": "city",
+        "places": "",
+        "album": "http://imgur.com/a/WJnS3",
+        "blog":"",
+        "albumId":"WJnS3"
+      },
+     "Cozumel":{
+        "name":"Cozumel",
+        "date":"January 3rd 2011",
+        "description":"a description",
+        "thumbnail":"http://i.imgur.com/jFY8k8Kh.jpg",
+        "rank": 20,
+        "tags": "cruise",
+        "places": "",
+        "album": "http://imgur.com/a/h8Svy",
+        "blog":"",
+        "albumId":"h8Svy"
+      },
+      "AbuDhabiIndia":{
+        "name":"Abu Dhabi/India",
+        "date":"May 8th 2010",
+        "description":"a description",
+        "thumbnail":"http://i.imgur.com/feiBatGh.jpg",
+        "rank": 20,
+        "tags": "tourism,hiking",
+        "places": "",
+        "album": "http://imgur.com/a/Y6Ww7",
+        "blog":"",
+        "albumId":"Y6Ww7"
+      }
+	},
+	initialize: function(){
+		Travel.loadPlaces(Travel.places);
+		Travel.loadEvents();
+	},
+	loadEvents: function(){
+		$('.place').hover(Travel.showContent);		
+		$('.place').on('mousemove', Travel.panImage);
+        $('.album-btn').click(Travel.getImgurAlbumData);
+        $('.scrolltop-btn').click(Travel.scrollTop); 
+        $(window).scroll(Travel.scrollListener); 
+	},
+	showContent: function(){
+		$(this).find('.content').toggleClass('content-visible');
+		$(this).find('.background').toggleClass('background-covered');
+	},
+	loadPlaces: function(places){
+		for (var key in places) {
+			var place = places[key];
+			Travel.loadPlace(key.toLowerCase(), place);
+		}
+	},
+    loadPlace:function(key, place){
+        $('#places').append('<div id="'+key+'" class="place wow fadeIn">'+
+            '<div class="content">'+
+            '<h2 class="place-title">'+place.name+'</h2>'+
+            '<span class="place-date">'+place.date+'</span>'+
+            (place.thumbnail ? 
+                '<div class="buttons">'+
+                '<div class="album-btn" album-id="'+place.albumId+'">Album</div>'+
+                (place.blog ? 
+                    '<a href="'+place.blog+'"><div class="blog-btn">Blog</div></a>' : '')+
+                '</div></div>'+
+                '<div class="background" style="background: url('+place.thumbnail+');"></div>' : 'COMING SOON')+
+            '</div>'
+        );  
+    },
+	panImage: function(e){
+      $(this).children('.background').css({
+      	'transform-origin': ((e.pageX - $(this).offset().left) / $(this).width()) * 100 + 
+      	'% ' + ((e.pageY - $(this).offset().top) / $(this).height()) * 100 +'%'      	
+      });
+	},
+    getImgurAlbumData: function() {         
+         Travel.toggleViews();
+         Travel.resetAlbum();
+         Travel.scrollTop();    
+         $.ajax({
+            url:'https://api.imgur.com/3/album/' + $(this).attr('album-id'),
+            headers: {"Authorization":"Client-ID a52210b6349e55a"},
+            type: 'GET',
+            dataType: 'json',
+            error: function() {
+                console.error('unable to retrieve album from Imgur...');
+            },
+            success: function(album) {
+                Travel.loadAlbum(album.data);
+            }
+        });
+    },
+    loadAlbum: function(album){
+        Travel.albumImages = album.images;
+        $('#album').addClass('album-loaded');
+        $('#album').empty();              
+        $('#album').append('<h4 class="album-title"></h4><div class="album-info"><div class="close-album-btn">Close Album</div>'+
+            '<div class="imgur-album-btn"><a href="'+album.link+'">Imgur</a></div></div>');
+        $('.album-title').html(album.title + ' ('+album.images.length+' images)');
+        var maxLoad = (Travel.albumImages.length < Travel.maxPage) ? Travel.albumImages.length : Travel.maxPage;        
+        for (var i=0;i<maxLoad;i++) {
+             var albumObj = Travel.albumImages[i];            
+             var imgUrl = albumObj.link;
+             var thumbnail = albumObj.link;
+             thumbnail = thumbnail.replace('.jpg', 'h.jpg');
+             thumbnail = thumbnail.replace('http', 'https');
+             Travel.loadAlbumImg(i, thumbnail, imgUrl);
+        }
+        if(Travel.albumImages.length > Travel.maxPage) {
+            $('#album').append('<div class="load-more-btn">Load More</div>');
+        }        
+        $('.album-img-overlay').click(Travel.loadAlbumImgSelected);
+        $('.close-album-btn').click(Travel.closeAlbum); 
+        Travel.loadAlbumEvents();
+    },
+    loadAlbumImg: function(index, thumbnail, url){
+        $('#album').append(                
+                '<div class="album-img" style="background: url('+thumbnail+');">'+
+                '<a href="'+url+'" class="link-btn">'+
+                '<div>'+
+                '<i class="fa fa-2x fa-picture-o"></i>'+
+                '</div>'+
+                '</a>'+
+                '<div class="album-img-overlay" album-img-index="'+index+'" img-url="'+url+'">'+                                                
+                '</div>'+
+                '</div>'                
+              );
+    },
+    loadAlbumEvents: function(){
+        document.onkeydown = function(e) {
+            switch (e.keyCode) {
+                case 37:
+                    if($('.selected-img')[0]){
+                        Travel.loadPreviousImage();
+                    } else {
+                        if($('.album-grid').children().size() > 0){
+                            Travel.closeAlbum();    
+                        }                        
+                    }
+                    break;
+                case 39:
+                    if($('.selected-img')[0]){
+                        Travel.loadNextImage();
+                    } 
+                    break;
+                case 27:
+                    if($('.selected-img')[0]){
+                        Travel.closeSelectedImage();
+                    } else {
+                        if($('.album-grid').children().size() > 0){
+                            Travel.closeAlbum();    
+                        }  
+                    }
+                    break;
+            }
+        };
+        $('.load-more-btn').click(function(){
+            var offset = $('.album-img').size()
+            if( offset < Travel.albumImages.length){
+                var maxLoad = (Travel.albumImages.length - offset < Travel.maxPage) ? Travel.albumImages.length  - offset :  Travel.maxPage;                
+                for (var i=offset;i<(offset+maxLoad);i++) {
+                    var albumObj = Travel.albumImages[i];            
+                    var imgUrl = albumObj.link;
+                    var thumbnail = albumObj.link;
+                    thumbnail = thumbnail.replace('.jpg', 'h.jpg');
+                    thumbnail = thumbnail.replace('http', 'https');
+                    Travel.loadAlbumImg(i, thumbnail, imgUrl);
+                }
+                if(maxLoad < Travel.maxPage) {
+                    $('.load-more-btn').remove();
+                }
+            }
+        });
+    },
+    loadNextImage: function(){         
+        var index = $('.selected-img').attr('index');
+        if(++index > Travel.albumImages.length-1){
+            index = 0;
+        }                        
+        var imgUrl = Travel.albumImages[index].link;
+        var bgStyle = 'rgba(0, 0, 0, 0) url("'+imgUrl+'") no-repeat scroll 50% 50% / 100% padding-box border-box';
+        $('.selected-img').css('background', bgStyle);                        
+        $('.selected-img').attr('index',index);
+    },
+    loadPreviousImage: function(){        
+        var index = $('.selected-img').attr('index');
+        if(--index < 0){
+            index = Travel.albumImages.length-1;
+        }                                                
+        var imgUrl = Travel.albumImages[index].link;
+        var bgStyle = 'rgba(0, 0, 0, 0) url("'+imgUrl+'") no-repeat scroll 50% 50% / 100% padding-box border-box';
+        $('.selected-img').css('background', bgStyle);                        
+        $('.selected-img').attr('index',index);
+    },
+    resetAlbum: function(){
+        $('#album').empty();
+        setTimeout(function(){
+            if(!$('#album').hasClass('album-loaded')){
+                $('#album').append('<span class="album-loading"><i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i></span>');
+            }
+        }, 1000); 
+    },
+    closeAlbum: function(){
+        $('#header').show();
+        Travel.toggleViews();
+        setTimeout(function(){
+            $('#album').empty();
+            $('#album').removeClass('album-loaded');    
+        }, 500); 
+    },
+    toggleViews: function(){
+        $('#album').toggleClass('album-hidden');
+        $('#places').toggleClass('places-hidden');        
+    },
+    scrollListener: function (event) {
+        if($(window).scrollTop() > 100){
+            $('.scrolltop-btn').removeClass('scrolltop-btn-hidden');
+        } else {
+            $('.scrolltop-btn').addClass('scrolltop-btn-hidden');
+        }
+    },
+    scrollTop: function(){
+        $('body').animate({ scrollTop: 0 }, "slow");
+    },
+    loadAlbumImgSelected: function(){
+        $('#album-img-selected').removeClass('album-img-selected-hidden');
+        $('#album-img-selected').append('<div class="selected-img" index="'+$(this).attr('album-img-index')+'" style="background: url('+$(this).attr('img-url')+');"></div>');
+        $('#album-img-selected').append('<span class="close-selected-img-btn"><i class="fa fa-close fa-3x"></i></span>');
+        $('#album-img-selected').append('<div class="open-img-nav"><i class="fa fa-fw fa-arrow-circle-left fa-3x"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-fw fa-arrow-circle-right fa-3x"></i></div>');
+        $('html').addClass('lock-scroll');
+        $('.close-selected-img-btn').click(Travel.closeSelectedImage);
+        Travel.loadAlbumImgSelectedEvents();
+    },
+    loadAlbumImgSelectedEvents: function(){
+        $('.open-img-nav > .fa-arrow-circle-left').click(function(){
+            Travel.loadPreviousImage();
+        });
+        $('.open-img-nav > .fa-arrow-circle-right').click(function(){
+            Travel.loadNextImage();
+        });
+    },
+    closeSelectedImage: function(){
+        $('#album-img-selected').addClass('album-img-selected-hidden');
+        setTimeout(function(){
+            $('#album-img-selected').empty();
+        },1000);
+        $('html').removeClass('lock-scroll');
+    }
+};
+
+Travel.initialize();
